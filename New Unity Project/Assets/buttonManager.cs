723972f439630fb7880290e06c217e74dea5f215
hamdanhasaini <hamdanhasaini@gmail.com>
2018-01-22 00:57:32 +0700

    project2

--- New Unity Project/Assets/buttonManager.cs
+++ New Unity Project/Assets/buttonManager.cs
@@ -1,16 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class buttonManager : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public void song1Btn(string newsong1) {
+        SceneManager.LoadScene(newsong1);
+    }
+    public void song2Btn(string newSong2)
+    {
+        SceneManager.LoadScene(newSong2);
+    }
+    public void song3Btn(string newSong3)
+    {
+        SceneManager.LoadScene(newSong3);
+    }
+
+    public void menuBtn(string menu) {
+        SceneManager.LoadScene(menu);
+    }
+
+    public void exitBtn() {
+        Application.Quit();
+    }
+
+
+
 }
